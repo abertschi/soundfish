2e3b5bf1527e29a4c9592a6e0d2d999a4368c562
Andrin Bertschi <andrin.bertschi@gmail.com>
2015-09-23 23:55:44 +0200

    clean up

--- python/sc_test.py
+++
@@ -1,9 +0,0 @@
-import soundcloud
-
-def getPermalink():
-    """Test SoundCloud API"""
-
-    client = soundcloud.Client(client_id="xxx")
-    app = client.get('/apps/124')
-
-    return app.permalink_url
